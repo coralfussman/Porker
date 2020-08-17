a84531811e322e1bc871ef09b882ce5cf7021f57
coralfussman <coralfussman@gmail.com>
2020-08-17 19:20:12 -0400

    found more piflatin rules, updated accordingly using regex

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ import { preventAutoHide } from "expo/build/launch/SplashScreen";
 import { Input, withTheme } from "react-native-elements";
 
 export default function App() {
-  console.log("App executed");
+  // console.log("App executed");
   // console.log(Dimensions.get("screen"));
   const [message, setMessage] = useState("");
 
@@ -27,24 +27,50 @@ export default function App() {
   const handleReset = () => {
     setMessage("");
   };
-  const handleSubmit = () => {
-    handleTranslate(message);
-    setMessage(handleTranslate);
-    // handleTranslate(message);
-  };
-  const handleTranslate = (string) => {
-    const firstVowel = string.match(/[aeiou]/);
-    const firstIndex = string.indexOf(firstVowel);
-    const trans = "";
-    if (firstIndex > 0) {
-      return (
-        string.toLowerCase().slice(firstIndex) +
-        string.slice(0, firstIndex) +
-        "ay"
-      );
-    } else if (!string === "") {
-      return string + "way";
+  // const handleSubmit = () => {
+  //   handleTranslate(message);
+  //   setMessage(handleTranslate);
+  //   // handleTranslate(message);
+  // };
+
+  const handleWords = (message) => {
+    message = message.toLowerCase();
+    message = message.replace(/ ?([.?!,]) ?/g, " $1 ");
+    var array = message.split(" ");
+    var word = "";
+    for (var i = 0; i < array.length; i++) {
+      word = array[i];
+      if (isWord(word)) array[i] = handleTranslate(array[i]);
     }
+    message = array.join(" ");
+    message.replace(/ ([!?.,]+)/g, "$1");
+    return message;
+  };
+
+  function isWord(text) {
+    return text.match(/[^a-zA-Z\-']/gi) === null;
+  }
+
+  const handleTranslate = (string) => {
+    if (string.substring(0, 1).match(/[aeiou]/gi) !== null)
+      return string + "yay";
+    // if they don't match, move 1st group of consonants to end and append ay
+
+    return string.replace(/([b-df-hj-np-tv-z]+)(.+)/gi, "$2$1ay");
+
+    // first solution
+    // const firstVowel = string.match(/[aeiou]/);
+    // const firstIndex = string.indexOf(firstVowel);
+    // const trans = "";
+    // if (firstIndex > 0) {
+    //   return (
+    //     string.toLowerCase().slice(firstIndex) +
+    //     string.slice(0, firstIndex) +
+    //     "ay"
+    //   );
+    // } else if (!string === "") {
+    //   return string + "way";
+    // }
   };
 
   return (
@@ -68,7 +94,6 @@ export default function App() {
         }}
       />
       <TextInput
-        numberOfLines={5}
         style={styles.input}
         placeholder="enter your message"
         value={message}
@@ -77,7 +102,7 @@ export default function App() {
       />
       <Text style={styles.text} placeholder="Oink Oink">
         {" "}
-        Translation Here: {handleTranslate(message)}
+        Translation Here: {handleWords(message)}
       </Text>
       {/* <Button
         style={buttonStyles.button}
